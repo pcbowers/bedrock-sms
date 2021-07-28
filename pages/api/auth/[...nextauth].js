45cbd372b16cb3d6a5941ee9260a0d941b508934
P. Christopher Bowers <pcbowers@gmail.com>
2021-07-28 09:34:37 -0400

    updated auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,26 +1,28 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+import { getAdmin } from "../../../lib/airtable_functions"
 
 const options = {
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-        Providers.Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_SECRET_ID
-        }),
-    ],
-    callbacks: {
-        async signIn(user, account, profile) {
-            const results = await (await fetch(`${process.env.NEXTAUTH_URL}/api/admins/${user.email}`)).json()
-            if (results.body) {
-                return true
-            } else {
-                return false
-            }
-        }
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET_ID
+    })
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const results = await getAdmin(user.email)
+      console.log(results)
+      if (results.email) {
+        return true
+      } else {
+        return false
+      }
     }
+  }
 }
 
 export default function auth(req, res) {
-    return NextAuth(req, res, options)
+  return NextAuth(req, res, options)
 }
